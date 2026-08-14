cf227f6c2e8d3f62ab7a648321ef576a508a8037
Jaaystones <Joelpaul345@gmail.com>
2026-08-14 12:04:58 +0100

    fix: add .gitignore, fix pnpm-workspace.yaml for Prisma builds, untrack artifacts

--- dist/types/user.types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toPublicUser = toPublicUser;
-function toPublicUser(user) {
-    const { passwordHash: _passwordHash, ...publicUser } = user;
-    return publicUser;
-}
-//# sourceMappingURL=user.types.js.map
